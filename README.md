fd4b73bf210554458366edff8aeb4aecd26c59d7
collinsnji <developer@collingrimm.me>
2020-02-22 14:23:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<!--
+ Copyright (c) 2020 Collin Grimm
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
---
+++ config.rb
@@ -0,0 +1,11 @@
+=begin
+ Copyright (c) 2020 Collin Grimm
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+=end
+
+css_dir = "style/css"
+sass_dir = "style/scss"
+
+line_comments = false
